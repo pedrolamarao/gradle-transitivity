dd862325e62959b4cfc9ab19e03d75d9adafc5d6
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-09-28 13:18:24 -0300

    Add CustomVisibility with appropriate consumable configurations.

--- buildSrc/src/main/kotlin/custom.gradle.kts
+++ buildSrc/src/main/kotlin/custom.gradle.kts
@@ -2,10 +2,26 @@ import org.gradle.kotlin.dsl.getValue
 import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.registering
 
+enum class CustomVisibility {
+    COMPILE,
+    RUN;
+}
+
+val CUSTOM_VISIBILITY_ATTRIBUTE = Attribute.of("custom.visibility",CustomVisibility::class.java)
+
 val customApi by configurations.registering {
     description = "custom api dependencies"
     isCanBeConsumed = false
     isCanBeResolved = false
+}
+
+val customApiElements by configurations.registering {
+    description = "custom api elements"
+    isCanBeConsumed = true
+    isCanBeResolved = false
+    attributes {
+        attribute(CUSTOM_VISIBILITY_ATTRIBUTE,CustomVisibility.COMPILE)
+    }
 }
 
 val customImplementation by configurations.registering {
@@ -13,21 +29,34 @@ val customImplementation by configurations.registering {
     extendsFrom(customApi.get())
     isCanBeConsumed = false
     isCanBeResolved = false
-    isVisible = false
 }
 
-val customCompile by configurations.registering {
+val customImplementationElements by configurations.registering {
+    description = "custom implementation elements"
+    extendsFrom(customImplementation.get())
+    isCanBeConsumed = true
+    isCanBeResolved = false
+    attributes {
+        attribute(CUSTOM_VISIBILITY_ATTRIBUTE,CustomVisibility.RUN)
+    }
+}
+
+val customCompileDependencies by configurations.registering {
     description = "custom compile dependencies"
     extendsFrom(customImplementation.get())
     isCanBeConsumed = false
     isCanBeResolved = true
-    isVisible = false
+    attributes {
+        attribute(CUSTOM_VISIBILITY_ATTRIBUTE,CustomVisibility.COMPILE)
+    }
 }
 
-val customRuntime by configurations.registering {
+val customRunDependencies by configurations.registering {
     description = "custom runtime dependencies"
     extendsFrom(customApi.get(),customImplementation.get())
     isCanBeConsumed = false
     isCanBeResolved = true
-    isVisible = false
+    attributes {
+        attribute(CUSTOM_VISIBILITY_ATTRIBUTE,CustomVisibility.RUN)
+    }
 }
